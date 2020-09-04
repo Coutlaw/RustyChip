09ae1fa6b14e6fe7f1bf0a66c84f99d4bff228c3
Coutlaw <cass.d.outlaw@gmail.com>
2020-09-03 21:56:38 -0400

    updates to process_opcode

--- src/cpu.rs
+++ src/cpu.rs
@@ -25,6 +25,7 @@ pub struct Cpu {
     pub dt: u8,
 }
 
+// a word is 16 bits, so we combine two 8 bit chunks of memory to form one word
 fn read_word(memory: [u8; 4096], index: u16) -> u16 {
     (memory[index as usize] as u16) << 8 | (memory[(index + 1) as usize] as u16)
 }
@@ -70,14 +71,34 @@ impl Cpu {
         }
     }
 
-    /*
-    nnn or addr - A 12-bit value, the lowest 12 bits of the instruction
-    n or nibble - A 4-bit value, the lowest 4 bits of the instruction
-    x - A 4-bit value, the lower 4 bits of the high byte of the instruction
-    y - A 4-bit value, the upper 4 bits of the low byte of the instruction
-    kk or byte - An 8-bit value, the lowest 8 bits of the instruction
-    */
     fn process_opcode(&mut self, opcode: u16) {
-        
+
+        // opcode params
+        // take the op code, mask its position, shift to the 0th place of the instruction
+
+        // x - A 4-bit value, the lower 4 bits of the high byte of the instruction
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+
+        // y - A 4-bit value, the upper 4 bits of the low byte of the instruction
+        let y = ((opcode & 0x00F0) >> 4) as usize;
+
+        // nnn or addr - A 12-bit value, the lowest 12 bits of the instruction
+        let nnn = (opcode & 0x0FFF);
+
+        // n or nibble - A 4-bit value, the lowest 4 bits of the instruction
+        let n = ((opcode & 0x000F)) as u8;
+
+        // kk or byte - An 8-bit value, the lowest 8 bits of the instruction
+        let kk = (opcode & 0x00FF) as u8;
+
+        // extract the smaller nibbles
+        let op_1 = ((opcode & 0xF000) >> 12) as u8;
+        let op_2 = ((opcode & 0x0F00) >> 8)as u8;
+        let op_3 = ((opcode & 0x00F0) >> 4)as u8;
+        let op_4 = (opcode & 0x000F) as u8;
+
+        // TODO: match opcodes to instructions
+
+
     }
 }
