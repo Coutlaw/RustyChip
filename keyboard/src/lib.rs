4e8e57d7f1ebad77c1d2d1c6479ced0508bb9979
Coutlaw <cass.d.outlaw@gmail.com>
2020-10-05 18:02:59 -0400

    keyboard impl

--- keyboard/src/lib.rs
+++ keyboard/src/lib.rs
@@ -1,3 +1,32 @@
+pub struct Keyboard { 
+    pub keys: [bool; 16],
+}
+
+impl Keyboard {
+    pub fn new() -> Keyboard {
+        Keyboard {
+            keys: [false; 16]
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.keys = [false; 16];
+    }
+
+    pub fn press_key(&mut self, key: u8) {
+        self.keys[key as usize] = true;
+    }
+
+    pub fn unpress_key(&mut self, key: u8) {
+        self.keys[key as usize] = false;
+    }
+
+    pub fn key_is_pressed(&self, key: u8) -> bool {
+        self.keys[key as usize]
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
