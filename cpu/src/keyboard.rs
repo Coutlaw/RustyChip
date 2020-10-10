201a36c54da5e80350f4b1e1af74152efc399c81
Coutlaw <cass.d.outlaw@gmail.com>
2020-10-10 07:28:20 -0400

    added a function to map indexes of chip 8 keyboard to my keybaord

--- cpu/src/keyboard.rs
+++ cpu/src/keyboard.rs
@@ -5,25 +5,35 @@ pub struct Keyboard {
 
 impl Keyboard {
 	pub fn new() -> Keyboard {
-			Keyboard {
-					keys: [false; 16]
-			}
+		Keyboard {
+			keys: [false; 16]
+		}
 	}
 
 	pub fn reset(&mut self) {
-			self.keys = [false; 16];
+		self.keys = [false; 16];
 	}
 
 	pub fn press_key(&mut self, key: u8) {
-			self.keys[key as usize] = true;
+		self.keys[key as usize] = true;
 	}
 
 	pub fn un_press_key(&mut self, key: u8) {
-			self.keys[key as usize] = false;
+		self.keys[key as usize] = false;
 	}
 
 	pub fn key_is_pressed(&self, key: u8) -> bool {
-			self.keys[key as usize]
+		self.keys[key as usize]
+	}
+
+	// convert a keypress into a chip 8 key value if possible
+	fn char_to_key(key: char) -> Option<u8> {
+		if let Some(x) = key.to_digit(10) {
+			if x <= 16 {
+				return Some(x as u8)
+			}
+		}
+		None
 	}
 }
 
@@ -32,6 +42,6 @@ impl Keyboard {
 mod tests {
 	#[test]
 	fn it_works() {
-			assert_eq!(2 + 2, 4);
+		assert_eq!(2 + 2, 4);
 	}
 }
