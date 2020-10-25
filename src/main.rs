89d2d4e52958f4e145f3eeebdea21caa8f96ceb9
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-10-25 08:27:48 -0400

    outline for the main game loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
 use cpu::Cpu;
 fn main() {
     let test =  Cpu::new();
+
+//     // Set up render system and register input callbacks
+//   setupGraphics();
+//   setupInput();
+ 
+//   // Initialize the Chip8 system and load the game into the memory  
+//   myChip8.initialize();
+//   myChip8.loadGame("pong");
+ 
+//   // Emulation loop
+//   for(;;)
+//   {
+//     // Emulate one cycle
+//     myChip8.emulateCycle();
+ 
+//     // If the draw flag is set, update the screen
+//     if(myChip8.drawFlag)
+//       drawGraphics();
+ 
+//     // Store key press state (Press and Release)
+//     myChip8.setKeys();	
+//   }
 }
 
 
